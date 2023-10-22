aef77f0f9bad508c7710209616dc078ef6a0fcc7
marcocslima@gmail.com <marcocslima@gmail.com>
2023-10-21 23:06:35 -0300

    feat: ex04

--- Day02/ex04/elem.py
+++ Day02/ex04/elem.py
@@ -30,7 +30,7 @@ class Elem:
         """
         self.tag = tag
         self.attr = attr
-        self.content = content
+        self.content = content if content is not None else []
         self.tag_type = tag_type
 
     def __str__(self):
@@ -89,40 +89,19 @@ class Elem:
 
 
 if __name__ == '__main__':
-    # title = Elem(tag='title', content='Hello ground', tag_type='double')
-    # h1 = Elem(tag='h1', content='Oh no, not again!', tag_type='double')
-    # img = Elem(tag='img', attr={'src': 'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')
+    title = Elem(tag='title', content='Hello ground', tag_type='double')
+    h1 = Elem(tag='h1', content='Oh no, not again!', tag_type='double')
+    img = Elem(tag='img', attr={'src': 'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')
     
-    # head = Elem(tag='head', content=[title],tag_type='double')
-    # body = Elem(tag='body', content=[h1, img], tag_type='double')
-    # html = Elem(tag='html', content=[head, body], tag_type='double')
-
-    # base = open('resp.html', 'w')
-    # base.write(str(html))
-    # base.close()
-    # Criar os elementos para cabeçalho, parágrafo e lista não ordenada
-    header = Elem(tag='h1', content='Meu Título', tag_type='double')
-    paragraph = Elem(tag='p', content='Este é um parágrafo de exemplo.', tag_type='double')
-    unordered_list = Elem(tag='ul', tag_type='double')
-
-    # Criar os elementos da lista não ordenada (itens de lista)
-    list_item_1 = Elem(tag='li', content='Item 1', tag_type='double')
-    list_item_2 = Elem(tag='li', content='Item 2', tag_type='double')
-    list_item_3 = Elem(tag='li', content='Item 3', tag_type='double')
-
-    # Adicionar os itens de lista à lista não ordenada usando add_content
-    # unordered_list.add_content([list_item_1, list_item_2, list_item_3])
-    unordered_list.add_content(list_item_1)
-
-    # Criar o elemento <body> e adicionar o cabeçalho, parágrafo e lista não ordenada usando add_content
-    body = Elem(tag='body', content=[header, paragraph])
-    body.add_content(unordered_list)
-
-    # Criar o elemento HTML e adicionar o cabeçalho e o corpo
-    html = Elem(tag='html', content=[body], tag_type='double')
-
-    # Agora você pode criar o arquivo HTML e gravar o HTML resultante nele
-    with open('exemplo.html', 'w') as file:
-        file.write(str(html))
-
     
+    header = Elem(tag='head',tag_type='double')
+    body = Elem(tag='body', tag_type='double')
+    html = Elem(tag='html', tag_type='double')
+
+    header.add_content(title)
+    body.add_content([h1, img])
+    html.add_content([header, body])
+
+    base = open('base.html', 'w')
+    base.write(str(html))
+    base.close()
